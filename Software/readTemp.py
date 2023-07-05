07ae22a83e5e5f304f3a5835c5724cefee57d84c
Ho Yun "Bobby <ho.chan@colorado.edu>
2023-07-05 15:01:52 -0600

    Update readTemp.py
    Use the `display.scroll()` function to display temperature better instead off `display.show()`.

--- Software/readTemp.py
+++ Software/readTemp.py
@@ -7,5 +7,5 @@ from microbit import *
 while True:
     rawTemp = pin2.read_analog()
     degC = (((rawTemp*3.3)/(1023))-0.5)*100
-    display.show(str(degC))
+    display.scroll(str(degC))
     sleep(1000)
